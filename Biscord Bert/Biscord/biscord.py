483c1a1f3ba39792671efa9dbd8201072d3bd0bf
Peeklecks04 <alexcavemann@gmail.com>
2022-03-22 14:14:03 +1030

    Unzipped some files, making it a little easier to run
    a

---
+++ Biscord Bert/Biscord/biscord.py
@@ -0,0 +1,18 @@
+from flask import *
+import dataset
+
+app = Flask(__name__)
+db = dataset.connect('sqlite:///biscord.db')
+@app.route('/')
+def index ():
+    return render_template('index.html', posts=db['posts'])
+
+@app.route('/create_post', methods=['post'])
+def create_post():
+    post_dictionary = {
+        'message' : request.form['message']
+    }
+    db['posts'].insert(post_dictionary)
+
+    return redirect('/')
+app.run(debug=True)
