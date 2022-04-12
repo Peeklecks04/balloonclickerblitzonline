e7c50be5b1911ce041e4129a4924458280895224
Peeklecks04 <alexcavemann@gmail.com>
2022-04-12 14:54:43 +0930

    Login exists, more changes to most things

--- biscord.py
+++ biscord.py
@@ -4,7 +4,7 @@ import dataset
 app = Flask(__name__)
 db = dataset.connect('sqlite:///biscord.db')
 @app.route('/chatroom')
-def index ():
+def chatroom ():
     return render_template('caht.html', posts=db['posts'])
 
 @app.route('/create_post', methods=['post'])
@@ -17,8 +17,24 @@ def create_post():
     return redirect('/chatroom')
 
 @app.route('/leaderboard')
-def index ():
-    return render_template('leaderboard.html')
+def leaderboard ():
+    return render_template('leaderboard.html', posts=db['leaderboard'])
+
+@app.route('/login')
+def login ():
+    return render_template('login.html')
+
+@app.route('/create_score', methods=['post'])
+def create_score():
+    score_dictionary = {
+        'sendscore' : request.form['sendscore']
+    }
+    db['leaderboard'].insert(score_dictionary)
+
+
+@app.route('/')
+def home ():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
